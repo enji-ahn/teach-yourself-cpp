95428c67a154d3debd56d5cc6971af8f2defe275
Ahn, Seung-rye <seungrye@gmail.com>
2018-11-25 20:18:34 +0900

    add more sample projects
    * interface 를 통해서 mock 하는 방법에 관련된 샘플 추가.
    * std::any 를 사용하는 샘플 추가. (C++17 기능이라서, 컴파일러별로 빌드가 안될 수 있음)
    ** 따라서, 아직은 cmake 를 통해서는 빌드되지 않도록 막아 놓음

---
+++ src/platforms/win/Integrating Mocking with C++ Default Parameters/Integrating Mocking with C++ Default Parameters.cpp
@@ -0,0 +1,52 @@
+﻿// Integrating Mocking with C++ Default Parameters.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdio>
+
+class ILogger {
+public:
+	virtual void log(int value) const = 0;
+};
+
+class LoggerMock : public ILogger {
+public:
+	void log(int value) const override { /* do nothing */ };
+};
+
+class Logger : public ILogger {
+public:
+	void log(int value) const override { printf("value : %d\n", value); }
+};
+
+int f(int x, int y) {
+	return x * y;
+}
+
+int f1(int x, int y, const ILogger& logger) {
+	logger.log(x * y);
+	return x * y;
+}
+
+int f2(int x, int y, const ILogger& logger = Logger()) {
+	logger.log(x * y);
+	return x * y;
+}
+
+using LoggerSeam = ILogger;
+int f3(int x, int y, const LoggerSeam& logger = Logger()) {
+	logger.log(x * y);
+	return x * y;
+}
+
+int main()
+{
+	f (3, 4);
+	f1(3, 4, Logger());
+	f2(3, 4);
+	f3(3, 4);
+
+	std::getchar();
+    return 0;
+}
+
---
+++ src/platforms/win/std_any/std_any.cpp
@@ -0,0 +1,42 @@
+﻿// std_any.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <any>
+#include <iostream>
+#include <type_traits>
+
+template <typename T, typename Any, typename Visitor>
+auto any_visit1(Any&& any, Visitor visit)
+-> std::enable_if_t<std::is_same_v<std::any, std::decay_t<Any>>>
+{
+	if (any.type() == typeid(T)) {
+		visit(std::any_cast<T>(std::forward<Any>(any)));
+	}
+}
+
+int main() {
+	std::any a0(17);
+
+	any_visit1<int>(
+		a0,
+		[](auto value)
+	{
+		std::cout << "value=" << value << "\n";
+	});
+
+	std::cout << "To terminate program, hit enter..." << std::endl;
+	getchar();
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
