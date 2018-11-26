ec40b147e5b734ffb28c512b94e3a5d165d1fbed
Enji <seungrye@devguru.co.kr>
2018-11-26 18:14:42 +0900

    비동시 통신시 connection 이 맺어지는 타이밍을 확인하기 위한 샘플 프로그램을 작성함.
    * server 에서 callback 이 불리기 전에, 연결은 완료됨.
      * 즉, 연결이 완료된 이후에 callback 이 불림

---
+++ src/platforms/win/asio_check_when_the_connection_is_made/client.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+io_service client_ioservice;
+tcp::socket client_socket{ client_ioservice };
+
+void work_as_client(unsigned short port)
+{
+	tcp::endpoint ep(tcp::endpoint(address::from_string("127.0.0.1"), port));
+
+	client_socket.connect(ep);
+	std::cout << "connected" << std::endl;
+}
---
+++ src/platforms/win/asio_check_when_the_connection_is_made/server.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+io_service server_ioservice;
+tcp::socket server_socket{ server_ioservice };
+io_service::work server_worker{ server_ioservice };
+
+boost::shared_ptr<tcp::acceptor> server_acceptor;
+
+static void accept_handler(const boost::system::error_code &ec)
+{
+	if (ec)
+	{
+		std::cout << "failed to accept data : " << ec.message() << std::endl;
+		return;
+	}
+
+	boost::this_thread::sleep_for(boost::chrono::seconds(10));
+
+	std::cout << "connected. "<< std::endl;
+}
+
+void work_as_server(unsigned short port)
+{
+	tcp::endpoint ep{ tcp::v4(), port };
+	server_acceptor = boost::make_shared<tcp::acceptor>(server_ioservice, ep);
+
+	server_acceptor->listen();
+
+	auto io_thread = boost::make_shared<boost::thread>([=]() { server_ioservice.run(); });
+
+	server_acceptor->async_accept(server_socket, accept_handler);
+
+	io_thread->join();
+}
