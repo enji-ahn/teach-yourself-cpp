f2fa582838a19687205b32c41b768d8fdfe9170f
안승례 <seungrye@gmail.com>
2019-06-23 03:42:08 +0900

    remove useless code

--- src/platforms/any/aiming_bullet/main.cpp
+++ src/platforms/any/aiming_bullet/main.cpp
@@ -100,24 +100,6 @@ int main(void)
 #endif
 	}
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
     return EXIT_SUCCESS;
 }
 
