ccaa2cb9f45960e76ecce5424dc0ed8f8db2dfbf
안승례 <seungrye@gmail.com>
2019-06-23 03:42:32 +0900

    add initial game_logic code

---
+++ src/platforms/any/game_logic/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <cstdint>
+#include <cassert>
+#include <cmath>
+
+#include "game.hpp"
+
+int main(void)
+{
+	Game game;
+
+	game.run();
+
+    return EXIT_SUCCESS;
+}
+
