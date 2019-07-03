588a325208930fcd001ddc6935f49a115c1235f5
안승례 <seungrye@gmail.com>
2019-07-03 21:12:48 +0900

    study the cpp for copy constructor error

---
+++ src/platforms/any/copy_constructor/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct NoImplicitCopy {
+	NoImplicitCopy() = default;
+	explicit NoImplicitCopy(const NoImplicitCopy& ) = default;
+};
+
+NoImplicitCopy foo() {
+	NoImplicitCopy n;
+	return n;
+}
+
+void bar(NoImplicitCopy n)
+{ }
+
+int main(void)
+{
+	NoImplicitCopy  n;
+	NoImplicitCopy x(n);
+
+	n = foo();
+
+	bar(n);
+
+    return EXIT_SUCCESS;
+}
