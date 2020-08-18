e97b986a260b3ae2a97326f419e163618effab37
Ahn, Seung-rye <seungrye@gmail.com>
2020-08-18 21:08:10 +0900

    add insertion sort implement

---
+++ src/headers/stopwatch.hpp
@@ -0,0 +1,44 @@
+﻿#ifndef __STOPWATCH_HPP__
+#define __STOPWATCH_HPP__
+
+#include <chrono>
+#include <iostream>
+
+class stopwatch
+{
+public:
+    stopwatch();
+    ~stopwatch();
+    void elapsed();
+    void reset();
+
+private:
+    stopwatch(const stopwatch &) = delete;
+    stopwatch &operator=(const stopwatch &) = delete;
+
+private:
+    std::chrono::steady_clock::time_point _start;
+};
+
+inline stopwatch::stopwatch()
+{
+    _start = std::chrono::high_resolution_clock::now();
+}
+
+inline void stopwatch::elapsed()
+{
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = stop - _start;
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+}
+
+inline void stopwatch::reset()
+{
+    _start = std::chrono::high_resolution_clock::now();
+}
+
+inline stopwatch::~stopwatch()
+{
+}
+
+#endif
---
+++ src/platforms/any/insertion_sort/main.cpp
@@ -0,0 +1,83 @@
+﻿// BitOrder.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cstdint>
+#include <cstring>
+#include <vector>
+#include <future>
+#include <algorithm>
+#include <chrono>
+#include <random>
+#include <stopwatch.hpp>
+
+std::vector<int> init_vec(int loops, int count)
+{
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> distrib(0, 900000);
+
+    auto inputs = std::vector<int>();
+    inputs.reserve(loops * count);
+
+    std::vector<std::future<std::vector<int>>>
+        vec;
+    for (auto i = 0; i < loops; ++i)
+    {
+        vec.push_back(
+            std::async(
+                [&distrib, &gen, &count]() {
+                    auto v = std::vector<int>();
+                    v.reserve(count);
+                    for (auto i = 0; i < count; ++i)
+                        v.push_back(distrib(gen));
+                    return v;
+                }));
+    }
+
+    for (auto &v : vec)
+    {
+        auto part = v.get();
+        std::move(part.begin(), part.end(), std::back_inserter(inputs));
+    }
+
+    return inputs;
+}
+
+/**
+ * @brief 삽입 정렬을 수행합니다.
+ *
+ * @param inputs 입력값
+ */
+void insertion_sort(std::vector<int> inputs)
+{
+    const auto len = inputs.size();
+    auto value = 0;
+    auto lower_i = 0;
+    for (auto i = 2; i < len; ++i)
+    {
+        value = inputs[i];
+        lower_i = i;
+        while (lower_i && inputs[lower_i - 1] > value)
+        {
+            inputs[i] = inputs[lower_i - 1];
+            lower_i--;
+        }
+
+        inputs[lower_i] = value;
+    }
+}
+
+int main()
+{
+    stopwatch swatch;
+    auto inputs = init_vec(98, 100000);
+    swatch.elapsed();
+
+    swatch.reset();
+    insertion_sort(inputs);
+    swatch.elapsed();
+
+    getchar();
+}
