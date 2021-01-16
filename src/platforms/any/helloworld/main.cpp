49351aeb326288a4f4d7b44f19249c4f0d28d25b
Ahn, Seung-rye <seungrye@gmail.com>
2021-01-17 04:33:49 +0900

    helloworld 프로젝트 추가

---
+++ src/platforms/any/helloworld/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    vector<string> msg{"Hello", "C++", "World", "from", "VS Code!"};
+
+    for (const string& word : msg)
+    {
+        cout<<word<<" ";
+    }
+
+    cout<<endl;
+
+    return 0;
+}
