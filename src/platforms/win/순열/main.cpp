36b74c7fc84ec8091063848f5f4475a1d360d493
안승례 <seungrye@devguru.co.kr>
2024-01-30 21:39:26 +0900

    숫자로 넘어온 값들 처리 (틀린 답임)
    테스트 결과
    
    테스트 1 〉	실패 (시간 초과)
    테스트 2 〉	실패 (시간 초과)
    테스트 3 〉	실패 (시간 초과)
    테스트 4 〉	실패 (시간 초과)
    테스트 5 〉	실패 (시간 초과)
    테스트 6 〉	실패 (시간 초과)
    테스트 7 〉	통과 (0.03ms, 4.21MB)
    테스트 8 〉	통과 (0.03ms, 3.69MB)
    테스트 9 〉	통과 (0.03ms, 4.18MB)
    테스트 10 〉	통과 (0.03ms, 3.64MB)
    테스트 11 〉	실패 (0.03ms, 3.64MB)
    테스트 12 〉	통과 (0.02ms, 4.15MB)
    테스트 13 〉	통과 (0.02ms, 4.21MB)
    테스트 14 〉	통과 (0.02ms, 4.15MB)
    테스트 15 〉	통과 (0.02ms, 3.68MB)

--- src/platforms/win/순열/main.cpp
+++ src/platforms/win/순열/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+#include <string>
 #include <map>
 
 /// <summary>
@@ -20,40 +21,38 @@ static std::string join(std::vector<std::string> sv, int until = 0) {
 	return result;
 }
 
+static std::string join(std::vector<int> nv, int until = 0) {
+	std::string result;
+	for (const auto& n : nv) {
+		if (!--until) break;
+		result += std::to_string(n);
+	}
+	return result;
+}
 
-// 앞자리 순서로 배열후 조합한다.
-// 앞자리가 같은 숫자 그룹은 순열을 통해서 가장 큰 값으로 만든다.
+// 생각한 방법 : 
+//   앞자리 순서로 배열후 조합한다.
+//   앞자리가 같은 숫자 그룹은 순열을 통해서 가장 큰 값으로 만든다.
 
 int main() {
-	std::map<char, std::vector<std::string>> nmap;
+	std::map<int, std::vector<int>> nmap;
 	auto candidates = std::vector<std::string>(10); 
-	std::vector<std::string> sv = { "4", "321", "31", "300", "5"};
+	std::vector<int> sv = { 3, 30, 34, 5, 9 };
 	
 	// 앞자리가 같은 숫자 그룹으로 분류
-	for (auto& s : sv) nmap[s.at(0)].push_back(s);
+	for (auto& s : sv) nmap[std::to_string(s)[0] - '0'].push_back(s);
 
 	// 2개 이상 아이템이 있는 그룹의 경우, 정렬
 	for (auto& m : nmap) {
 		auto& numbers = m.second;
 		if (numbers.size() > 1) {
-			std::sort(std::begin(numbers), std::end(numbers),
-				[](std::string& left, std::string& right)->bool {
-					if (left.size() != right.size()) {
-						return left.size() > right.size();
-					}
-
-					for (auto i = 0; i < left.size(); ++i) {
-						if (left.at(i) > right.at(i)) return true;
-					}
-					return false;
-				}
-			);
+			std::sort(std::begin(numbers), std::end(numbers));
 		}
 	}
 
 	// 2개 이상 아이템이 있는 그룹의 순열을 획득하여, 가장 큰 값을 candidate 에 저장
 	for (auto& m : nmap) {
-		auto index = m.first - '0';
+		auto index = m.first;
 		auto& numbers = m.second;
 		if (numbers.size() > 1) {
 			candidates[index] = join(numbers);
@@ -70,22 +69,11 @@ int main() {
 						break;
 					}
 				}
-			} while (next_permutation(std::begin(numbers), std::end(numbers), 
-				[](std::string& left, std::string& right)->bool {
-					if (left.size() != right.size()) {
-						return left.size() > right.size();
-					}
-
-					for (auto i = 0; i < left.size(); ++i) {
-						if (left.at(i) > right.at(i)) return true;
-					}
-					return false;
-				}
-			));
+			} while (next_permutation(std::begin(numbers), std::end(numbers)));
 			std::cout << std::endl;
 		}
 		else {
-			candidates[index] = numbers.at(0);
+			candidates[index] = std::to_string(numbers.at(0));
 		}
 
 		std::cout << " max : " << candidates[index] << std::endl;
