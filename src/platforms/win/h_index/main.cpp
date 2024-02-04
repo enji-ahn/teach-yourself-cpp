35abb8c7e2a118372fac5198cb3b4f04c54152d0
안승례 <seungrye@devguru.co.kr>
2024-02-04 18:11:32 +0900

    인터넷에 있는 설명 참고하여 풂

--- src/platforms/win/h_index/main.cpp
+++ src/platforms/win/h_index/main.cpp
@@ -9,35 +9,34 @@
 
 /**
  * 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42747
+ * 이해 : https://school.programmers.co.kr/questions/64629 설명
  * 내가 생각한 풀이: 
- *   1. 정렬 수행 (오름차순 정렬)
- *   2. 가운 값을 잡고, 가운데 값을 포함하여 우측 갯수가 가운데 값과 같거나 크고,
-        촤특 값들은 모두 합한 값이 가운데 값 이하라면, 해당 값을 취득한다.
-        3 0 6 1 5 -> 0 1 3 5 6
-        [3]
-        우측 갯수 3
-        좌측 합 1
+ *   1. 정렬 수행 (내림차순 정렬)
+ *   2. index 를 올려가면서, 값과, 인용한 갯수중 작은 값을 answer 에 저장
+ *   4. 만약 값이 answer 보다 작다면, 이후는 볼 필요 없음.
+        3 0 6 1 5 -> 6 5 3 1 0
+        [6] : 인용 갯수 1 -> min(6, 1) => 1
+        [5] : 인용 갯수 2 -> min(5, 2) => 2
+        [3] : 인용 갯수 3 -> min(3, 3) => 3
+        2 는 answer 보다 작으므로, 더 이상 보지 않음. 3 반환.
 **/
 
 int solution(std::vector<int> citations) {
-    int answer = -1;
-    std::sort(std::begin(citations), std::end(citations));
-    int index = std::floor(citations.size()/2);
-    for (; index < citations.size(); ++index) {
-        int count = citations.size() - index;
-        if (count >= citations[index]) {
-            answer = citations[index];
-        }
-        else {
-            break;
-        }
+    int answer = 0;
+    std::sort(std::begin(citations), std::end(citations), std::greater<>{});
+    for (auto i = 0; i < citations.size(); ++i) {
+        if (answer > citations[i]) break;
+        auto h = std::min(citations[i], i + 1);
+        answer = std::max(h, answer);
     }
 
     return answer;
 }
 
 int main() {
-    auto r = solution({ 3, 0, 6, 1, 5 });
+    auto r = solution({ 9, 7, 6, 2, 1 });
+    std::cout << "r : " << r << std::endl;
+    r = solution({ 3, 0, 6, 1, 5 });
     std::cout << "r : " << r << std::endl;
 	return 0;
 }
